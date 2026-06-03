0306980d582d34f0bfeb83c2bc794aad449dace0
Aditya <adityas.130478@gmail.com>
2026-06-03 16:25:37 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit=0;
+        int minPrice = INT_MAX;
+        for(int price : prices){
+            if(price<minPrice){
+                minPrice=price;
+            }
+            if(price-minPrice>maxProfit){
+                maxProfit = price-minPrice;
+            }
+        }
+        return maxProfit;
+    }
+};
